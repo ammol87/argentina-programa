fbd88cff35c87d19a92fab3c05cd2666b4838a6f
Agustin Molinari <ammol87@hotmail.com>
2023-03-04 15:03:32 -0300

    sube todo

--- js/funcionalidad.js
+++ js/funcionalidad.js
@@ -2,71 +2,6 @@
 let foto, nombre, apellido, genero, identificacion, nroIdentificacion, pais, provincia, ciudad, calle, numero, codPostal;
 let zonaHoraria, hora, correoElect, fechaNac, edad, telMovil, telFijo, seed;
 
-
-$.ajax({
-    url: 'https://randomuser.me/api/?nat=es',
-    dataType: 'json',
-    success: function(data) {
-        /**
-         * console.log(data.results[0]);
-         */
-
-        foto = data.results[0].picture.large;
-        apellido= data.results[0].name.last;
-        nombre = data.results[0].name.first;
-
-        identificacion = data.results[0].id.name;
-        nroIdentificacion = data.results[0].id.value;
-        fechaNac = data.results[0].dob.date;
-        edad = data.results[0].dob.age;
-        pais = data.results[0].location.country;
-        provincia = data.results[0].location.state;
-        ciudad = data.results[0].location.city;
-        calle = data.results[0].location.street.name;
-        numeroCalle = data.results[0].location.street.number;
-        codPostal = data.results[0].location.postcode;
-        zonaHoraria = data.results[0].location.timezone.description;
-        hora = data.results[0].location.timezone.offset;
-        correoElect = data.results[0].email;
-        telMovil = data.results[0].cell;
-        telFijo = data.results[0].phone;
-
-        fotoPerfil.src=foto;
-        apellidoPersona.innerText=apellido.toUpperCase();
-        nombrePersona.innerText=nombre;
-
-        identificacionPersona.innerText=identificacion + " _ " + nroIdentificacion;
-        nacimientoPersona.innerText=fechaNac +" ("+edad+" años)";
-        residenciaPersona.innerText=ciudad+", "+provincia+", "+pais;
-        callePersona.innerText=calle +" "+numeroCalle;
-        codPostPersona.innerText=codPostal;
-        zonaHorariaPersona.innerText=zonaHoraria+ " ("+hora+")";
-        correoElectPersona.innerText=correoElect;
-        telMovilPersona.innerText=telMovil;
-        telFijoPersona.innerText=telFijo;
-
-        seed = data.info.seed;
-        
-    }
-});
-
-
-/**
- * script para mostrar u ocultar los datos de interés
- */
-let botones = document.getElementsByClassName('fleVisibilidad');
-for(let i=0; i<botones.length; i++){
-    botones[i].addEventListener('click', function () {
-        let identificador = botones[i].id;
-        let padre = botones[i].parentNode;
-        let abuelo= padre.parentNode;
-        let elementoVisibilidad = abuelo.children[1];
-        
-        mostrarOcultar(identificador, elementoVisibilidad);
-
-    }, false);
-};
-
 // Función para mostrar u ocultar botones
 
 function mostrarOcultar(identificador, elementoVisibilidad){
@@ -133,3 +68,68 @@ function mostrarOcultar(identificador, elementoVisibilidad){
             break;
     }
 }
+
+$.ajax({
+    url: 'https://randomuser.me/api/?nat=es',
+    dataType: 'json',
+    success: function(data) {
+        /**
+         * console.log(data.results[0]);
+         */
+
+        foto = data.results[0].picture.large;
+        apellido= data.results[0].name.last;
+        nombre = data.results[0].name.first;
+
+        identificacion = data.results[0].id.name;
+        nroIdentificacion = data.results[0].id.value;
+        fechaNac = data.results[0].dob.date;
+        edad = data.results[0].dob.age;
+        pais = data.results[0].location.country;
+        provincia = data.results[0].location.state;
+        ciudad = data.results[0].location.city;
+        calle = data.results[0].location.street.name;
+        numeroCalle = data.results[0].location.street.number;
+        codPostal = data.results[0].location.postcode;
+        zonaHoraria = data.results[0].location.timezone.description;
+        hora = data.results[0].location.timezone.offset;
+        correoElect = data.results[0].email;
+        telMovil = data.results[0].cell;
+        telFijo = data.results[0].phone;
+
+        fotoPerfil.src=foto;
+        apellidoPersona.innerText=apellido.toUpperCase();
+        nombrePersona.innerText=nombre;
+
+        identificacionPersona.innerText=identificacion + " " + nroIdentificacion;
+        nacimientoPersona.innerText=fechaNac.substring(0,10) +" ("+edad+" años)";
+        residenciaPersona.innerText=ciudad+", "+provincia+", "+pais;
+        callePersona.innerText=calle +" "+numeroCalle;
+        codPostPersona.innerText=codPostal;
+        zonaHorariaPersona.innerText=zonaHoraria+ " ("+hora+")";
+        correoElectPersona.innerText=correoElect;
+        telMovilPersona.innerText=telMovil;
+        telFijoPersona.innerText=telFijo;
+
+        seed = data.info.seed;
+        
+    }
+});
+
+
+
+/**
+ * script para mostrar u ocultar los datos de interés
+ */
+let botones = document.getElementsByClassName('fleVisibilidad');
+for(let i=0; i<botones.length; i++){
+    botones[i].addEventListener('click', function () {
+        let identificador = botones[i].id;
+        let padre = botones[i].parentNode;
+        let suppa= padre.parentNode;
+        let elementoVisibilidad = suppa.children[1];
+        
+        mostrarOcultar(identificador, elementoVisibilidad);
+
+    }, false);
+};
